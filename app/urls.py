c9da02f9a12698c73d8c3c0e998a09b95d54a7f2
NwabuezeDavid <davidnwabueze333@gmail.com>
2024-10-26 19:42:54 +0100

    first commit

---
+++ app/urls.py
@@ -0,0 +1,23 @@
+from app.views import *
+
+from django.urls import path
+urlpatterns = [
+    path("",home, name="home"),
+    path("about/",about, name="about"),
+    path("service/",service, name="service"),
+    path("contact/",contact, name="contact"),
+    path("donate/",donate, name="donate"),
+    path("partner/",partner, name="partner"),
+    path("pastjob/",pastjob, name="pastjob"),
+    path("press/",press, name="press"),
+    path("newdetail/",newdetail, name="newdetail"),
+    path("ourteam/",ourteam, name="ourteam"),
+    path("newstory/",newstory, name="newstory"),
+    path("project/",project, name="project"),
+    path("projectupdate/",projectupdate, name="projectupdate"),
+    path("publications/",publications, name="publications"),
+    path("report/",report, name="report"),
+    path("traning/",traning, name="traning"),
+    path("siteclinic/",siteclinic, name="siteclinic"),
+    path("projectupdate/",projectupdate, name="projectupdate"),
+]
---
+++ app/views.py
@@ -0,0 +1,93 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    conx={
+        
+    }
+    return render(request, "home.html")
+def about(request):
+    conx={
+        
+    }
+    return render(request, "about.html")
+def service(request):
+    conx={
+        
+    }
+    return render(request, "service.html")
+def contact(request):
+    conx={
+        
+    }
+    return render(request, "contact.html")
+def donate(request):
+    conx={
+        
+    }
+    return render(request, "donate.html")
+def partner(request):
+    conx={
+        
+    }
+    return render(request, "partner.html")
+def pastjob(request):
+    conx={
+        
+    }
+    return render(request, "pastjob.html")
+def press(request):
+    conx={
+        
+    }
+    return render(request, "press.html")
+def siteclinic(request):
+    conx={
+        
+    }
+    return render(request, "siteclinic.html")
+def ourteam(request):
+    conx={
+        
+    }
+    return render(request, "ourteam.html")
+def newdetail(request):
+    conx={
+        
+    }
+    return render(request, "newdetail.html")
+def newstory(request):
+    conx={
+        
+    }
+    return render(request, "newstory.html")
+def project(request):
+    conx={
+        
+    }
+    return render(request, "project.html")
+def projectupdate(request):
+    conx={
+        
+    }
+    return render(request, "projectupdate.html")
+def publications(request):
+    conx={
+        
+    }
+    return render(request, "publications.html")
+def report(request):
+    conx={
+        
+    }
+    return render(request, "report.html")
+def traning(request):
+    conx={
+        
+    }
+    return render(request, "traning.html")
+def projectupdate(request):
+    conx={
+        
+    }
+    return render(request, "projectupdate.html")
